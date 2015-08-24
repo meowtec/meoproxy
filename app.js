bd50320d2797591fd397961712406e1f67771fb1
Meowtec <bertonzh@gmail.com>
2015-08-24 15:12:33 +0800

    调通 HTTPS 明文代理

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ var http = require('http')
 var https = require('https')
 var net = require('net')
 var url = require('url')
-var ____ = require('./utils')
-var httpHandlers = require('./http-handlers')
+
+var ____ = require('./lib/utils')
+var httpHandlers = {
+  connect: require('./lib/handlers/connect'),
+  request: require('./lib/handlers/request')
+}
 
 var proxy = http.createServer()
 
 // HTTP 代理服务器
-proxy.on('request', httpHandlers.request)
+proxy.on('request', httpHandlers.request.bind(null, 'http'))
 proxy.on('connect', httpHandlers.connect)
 
 proxy.listen(8899, '127.0.0.1')
