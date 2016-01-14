8dc879da6a24e4033b0b374ab7e7ed50205e4876
Meowtec <bertonzh@gmail.com>
2016-01-14 17:31:24 +0800

    add code editor core.

---
+++ src/front/components/editor-core.tsx
@@ -0,0 +1,64 @@
+'use strict'
+
+import * as React from 'react'
+import * as _ from '../../utils/utils'
+
+const EditorMode = {
+  html: 'text/html',
+  css: 'text/css',
+  js: 'text/js'
+}
+
+export interface EditorProps {
+  mode: string
+  onChange(value: string, codeMirror: CodeMirror.Editor): void
+  defaultValue?: string
+}
+
+export default class Editor extends React.Component<EditorProps, any> {
+  cmConfig: CodeMirror.EditorConfiguration
+  codeMirror: CodeMirror.Editor
+
+  constructor(props) {
+    super(props)
+
+    this.cmConfig = {
+      value: props.defaultValue || '',
+      mode: EditorMode[props.mode],
+      indentUnit: 2,
+      indentWithTabs: true,
+      lineNumbers: true,
+      extraKeys: {
+        'Enter': 'newlineAndIndentContinueMarkdownList'
+      },
+      theme: 'neo'
+    }
+
+  }
+
+  componentDidMount() {
+    const element: any = this.refs['target']
+
+    setTimeout(() => {
+      let codeMirror = CodeMirror(element, this.cmConfig)
+      this.codeMirror = codeMirror
+
+      this.bindEvent()
+    }, 3000)
+  }
+
+  bindEvent() {
+    const codeMirror = this.codeMirror
+
+    codeMirror.on('change', (codeMirror) => {
+      this.props.onChange(codeMirror.getDoc().getValue(), codeMirror)
+    })
+  }
+
+  render() {
+    return (
+      <label className="text-editor" ref="target"></label>
+    )
+  }
+
+}
---
+++ static/lib/codemirror/release.md
@@ -0,0 +1,9 @@
+ * Improve the way overlay (OS X-style) scrollbars are handled
+
+ * Make [annotatescrollbar](http://codemirror.net/doc/manual.html#addon_annotatescrollbar) and scrollpastend addons work properly together
+
+ * Make [show-hint](http://codemirror.net/doc/manual.html#addon_show-hint) addon select options on single click by default, move selection to hovered item
+
+ * Properly fold comments that include block-comment-start markers
+
+ * Many small language mode fixes
