b67f058f80976e11806cd0c01a28d75df277d042
Meowtec <bertonzh@gmail.com>
2016-01-15 20:32:22 +0800

    add backend for request/response replacer

---
+++ src/server/replace.ts
@@ -0,0 +1,20 @@
+'use strict'
+
+import { EventEmitter } from 'events'
+import { ipcMain } from 'electron'
+import { Request, Response, Type } from '../typed/typed'
+
+const event = new EventEmitter()
+
+const eventName = (id, type) => id + '_' + type
+
+ipcMain.on('replaced', (sender, data: {
+  id: string
+  type: Type
+  data: Request | Response
+}) => {
+  event.emit(eventName(data.id, data.type), data.data)
+})
+
+export default (id: string, type: Type) => (data, handler) =>
+    new Promise(event.once.bind(event, eventName(id, type)))
