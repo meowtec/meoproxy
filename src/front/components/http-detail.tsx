5f7590c7b2b5a6b86507806788f29730dcb230e5
Meowtec <bertonzh@gmail.com>
2016-02-25 20:13:45 +0800

    fix style

--- src/front/components/http-detail.tsx
+++ src/front/components/http-detail.tsx
@@ -48,8 +48,14 @@ export default class HttpDetail extends React.Component<HttpDetailProps, any> {
     let data = this.props.data
 
     if (data) {
+      let response = data.response
       let requestHeaders = data.request.headers
-      let responseHeaders = data.response.headers
+
+      let responseHeaders = {}, status
+      if (response) {
+        ({ status } = response)
+        responseHeaders = response.headers
+      }
 
       return (
         <div className="detail-container">
@@ -62,38 +68,40 @@ export default class HttpDetail extends React.Component<HttpDetailProps, any> {
           <Page className="detail-content" value={this.state.selectedId}>
 
             <PageItem value="headers">
-              <Panel name="General">
-                <dt>Method</dt>
-                <dd>{data.request.method}</dd>
-                <dt>URL</dt>
-                <dd>{_.genUrl(data.protocol, data.request.hostname, data.request.port, data.request.path)}</dd>
-                <dt>Status</dt>
-                <dd>{data.response.status}</dd>
-              </Panel>
-              <Panel name="Request Headers">
-                {
-                  Object.keys(requestHeaders).map(key => {
-                    return (
-                      [
-                        <dt>{key}</dt>,
-                        <dd>{requestHeaders[key]}</dd>
-                      ]
-                    )
-                  })
-                }
-              </Panel>
-              <Panel name="Response Headers">
-                {
-                  Object.keys(responseHeaders).map(key => {
-                    return (
-                      [
-                        <dt>{key}</dt>,
-                        <dd>{responseHeaders[key]}</dd>
-                      ]
-                    )
-                  })
-                }
-              </Panel>
+              <div className="headers">
+                <Panel name="General">
+                  <dt>Method</dt>
+                  <dd>{data.request.method}</dd>
+                  <dt>URL</dt>
+                  <dd>{_.genUrl(data.protocol, data.request.hostname, data.request.port, data.request.path)}</dd>
+                  <dt>Status</dt>
+                  <dd>{status}</dd>
+                </Panel>
+                <Panel name="Request Headers">
+                  {
+                    Object.keys(requestHeaders).map(key => {
+                      return (
+                        [
+                          <dt>{key}</dt>,
+                          <dd>{requestHeaders[key]}</dd>
+                        ]
+                      )
+                    })
+                  }
+                </Panel>
+                <Panel name="Response Headers">
+                  {
+                    Object.keys(responseHeaders).map(key => {
+                      return (
+                        [
+                          <dt>{key}</dt>,
+                          <dd>{responseHeaders[key]}</dd>
+                        ]
+                      )
+                    })
+                  }
+                </Panel>
+              </div>
             </PageItem>
 
             <PageItem value="request">
