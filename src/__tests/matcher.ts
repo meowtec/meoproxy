455f3a9111c9e3a596bfff077eb878c15782dad8
Meowtec <bertonzh@gmail.com>
2016-06-23 12:21:09 +0800

    TWEAK

--- src/__tests/matcher.ts
+++ src/__tests/matcher.ts
@@ -4,7 +4,7 @@ import * as assert from 'assert'
 import Matcher from '../utils/matcher'
 
 describe('matcher', () => {
-  it('', () => {
+  it('test matcher', () => {
     const matcher = new Matcher('*.sample.com')
     assert.equal(matcher['regexp'].toString(), '/^.*\\.sample\\.com$/')
     assert.ok(matcher.test('www.sample.com'))
--- src/utils/matcher.ts
+++ src/utils/matcher.ts
@@ -1,24 +1,24 @@
 'use strict'
 
 // '.*^' -> /[\.\*\^]/
-function _chars2reg(chars: string) {
-  return new RegExp('[' + chars.split('').map(char => '\\' + char).join('') + ']', 'g')
-}
+const charsToReg = (chars: string) =>
+  new RegExp('[' + chars.split('').map(char => '\\' + char).join('') + ']', 'g')
 
-const str2reg = (() => {
+let str2reg
+{
   const defaultChars = '/\\^$-{}[]()*.?|'
-  const defaultReg = _chars2reg(defaultChars)
+  const defaultReg = charsToReg(defaultChars)
 
-  return (str: string, escape = '') => {
+  str2reg = (str: string, escape = '') => {
     let chars = defaultChars
     let reg = defaultReg
     if (escape) {
-      chars = chars.replace(_chars2reg(escape), '')
-      reg = _chars2reg(chars)
+      chars = chars.replace(charsToReg(escape), '')
+      reg = charsToReg(chars)
     }
     return '^' + str.replace(reg, '\\$&') + '$'
   }
-})()
+}
 
 export default class Matcher {
 
